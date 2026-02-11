00a1bc4d20a23b0dcb8cedc345a7acc028b7f321
furkantanyol <furkantanyol@gmail.com>
2026-02-11 14:49:46 +0300

    Update Homebrew formula SHA256 for v0.1.0

--- Formula/aitelier.rb
+++ Formula/aitelier.rb
@@ -2,7 +2,7 @@ class Aitelier < Formula
   desc "Your AI atelier - craft fine-tuned models"
   homepage "https://github.com/furkantanyol/aitelier"
   url "https://registry.npmjs.org/aitelier/-/aitelier-0.1.0.tgz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "8f960e9c0eaf32c0751e6141b14d07e3fa071214a305261581cac345090e046c"
   license "MIT"
 
   depends_on "node"
